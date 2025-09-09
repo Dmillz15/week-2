dfb313b8f177613d2ad19728e238caa4f6b8dbfc
Dmillz15 <160161559+Dmillz15@users.noreply.github.com>
2025-09-09 18:28:23 +0000

    Excercises for week 2

--- apputil.py
+++ apputil.py
@@ -3,11 +3,25 @@ import numpy as np
 
 # update/add code below ...
 
-def ways(n):
-    return None
+def ways(n: int):
+    combos = []
+    for nickels in range(n // 5 + 1):
+        pennies = n - 5 * nickels
+        combos.append((pennies, nickels))
+    return combos
+ways(100)
 
-def lowest_score(names, scores):
-    return None
+names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
+scores = np.array([99, 71, 85, 62, 91])
+
+def lowest_score(names, scores): 
+    idx = np.argmin(scores)   # index of the minimum score
+    return names[idx]
+
+print(lowest_score(names, scores))
 
 def sort_names(names, scores):
-    return None
+    sorted_idx = np.argsort(scores)[::-1]
+    return list(names[sorted_idx])
+
+print(sort_names(names, scores))
